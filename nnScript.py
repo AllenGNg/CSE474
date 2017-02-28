490798d7e4aebc7d17350de2daaa9cb4e3567f25
Allen Ng <allenng@buffalo.edu>
2017-02-27 21:24:47 -0500

    Added Sigmoid Function, changed preprocess.
    Changed preprocess to preprocess_small to make it easier to debug and test.

--- nnScript.py
+++ nnScript.py
@@ -29,6 +29,108 @@ def sigmoid(z):
     s = 1.0 / (1.0 + np.exp(-1.0 * z))
     return  s
 
+def preprocess(): # preprocess_small() from Piazza, added to use a smaller test instead of the original
+    """ Input:
+     Although this function doesn't have any input, you are required to load
+     the MNIST data set from file 'mnist_sample.mat'.
+
+     Output:
+     train_data: matrix of training set. Each row of train_data contains
+       feature vector of a image
+     train_label: vector of label corresponding to each image in the training
+       set
+     validation_data: matrix of training set. Each row of validation_data
+       contains feature vector of a image
+     validation_label: vector of label corresponding to each image in the
+       training set
+     test_data: matrix of training set. Each row of test_data contains
+       feature vector of a image
+     test_label: vector of label corresponding to each image in the testing
+       set
+
+     - feature selection"""
+
+
+    mat = loadmat('../basecode/mnist_sample.mat')
+        # ------------Initialize preprocess arrays----------------------#
+    train_preprocess = np.zeros(shape=(4996, 784))
+    validation_preprocess = np.zeros(shape=(1000, 784))
+    test_preprocess = np.zeros(shape=(996, 784))
+    train_label_preprocess = np.zeros(shape=(4996,))
+    validation_label_preprocess = np.zeros(shape=(1000,))
+    test_label_preprocess = np.zeros(shape=(996,))
+    # ------------Initialize flag variables----------------------#
+    train_len = 0
+    validation_len = 0
+    test_len = 0
+    train_label_len = 0
+    validation_label_len = 0
+    # ------------Start to split the data set into 6 arrays-----------#
+    for key in mat:
+        # -----------when the set is training set--------------------#
+        if "train" in key:
+            label = key[-1]  # record the corresponding label
+            tup = mat.get(key)
+            sap = range(tup.shape[0])
+            tup_perm = np.random.permutation(sap)
+            tup_len = len(tup)  # get the length of current training set
+            tag_len = tup_len - 100  # defines the number of examples which will be added into the training set
+
+            # ---------------------adding data to training set-------------------------#
+            train_preprocess[train_len:train_len + tag_len] = tup[tup_perm[100:], :]
+            train_len += tag_len
+
+            train_label_preprocess[train_label_len:train_label_len + tag_len] = label
+            train_label_len += tag_len
+
+            # ---------------------adding data to validation set-------------------------#
+            validation_preprocess[validation_len:validation_len + 100] = tup[tup_perm[0:100], :]
+            validation_len += 100
+
+            validation_label_preprocess[validation_label_len:validation_label_len + 100] = label
+            validation_label_len += 100
+
+            # ---------------------adding data to test set-------------------------#
+        elif "test" in key:
+            label = key[-1]
+            tup = mat.get(key)
+            sap = range(tup.shape[0])
+            tup_perm = np.random.permutation(sap)
+            tup_len = len(tup)
+            test_label_preprocess[test_len:test_len + tup_len] = label
+            test_preprocess[test_len:test_len + tup_len] = tup[tup_perm]
+            test_len += tup_len
+            # ---------------------Shuffle,double and normalize-------------------------#
+    train_size = range(train_preprocess.shape[0])
+    train_perm = np.random.permutation(train_size)
+    train_data = train_preprocess[train_perm]
+    train_data = np.double(train_data)
+    train_data = train_data / 255.0
+    train_label = train_label_preprocess[train_perm]
+
+    validation_size = range(validation_preprocess.shape[0])
+    vali_perm = np.random.permutation(validation_size)
+    validation_data = validation_preprocess[vali_perm]
+    validation_data = np.double(validation_data)
+    validation_data = validation_data / 255.0
+    validation_label = validation_label_preprocess[vali_perm]
+
+    test_size = range(test_preprocess.shape[0])
+    test_perm = np.random.permutation(test_size)
+    test_data = test_preprocess[test_perm]
+    test_data = np.double(test_data)
+    test_data = test_data / 255.0
+    test_label = test_label_preprocess[test_perm]
+
+    # Feature selection
+    # Your code here.
+    print('preprocess done')
+
+
+    return train_data, train_label, validation_data, validation_label, test_data, test_label
+
+
+"""
 def preprocess():
     """ Input:
      Although this function doesn't have any input, you are required to load
@@ -131,7 +233,7 @@ def preprocess():
     print('preprocess done')
 
     return train_data, train_label, validation_data, validation_label, test_data, test_label
-
+"""
 
 def nnObjFunction(params, *args):
     """% nnObjFunction computes the value of objective function (negative log
@@ -221,7 +323,7 @@ def nnPredict(w1, w2, data):
 
 train_data, train_label, validation_data, validation_label, test_data, test_label = preprocess()
 
-#  Train Neural Network
+#   Neural Network
 
 # set the number of nodes in input unit (not including bias unit)
 n_input = train_data.shape[1]
